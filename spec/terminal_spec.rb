e33802c0e4d5311bde767005c5fd7df4167adb07
Emily Schomer <minischomer@gmail.com>
2014-06-17 15:42:52 -0500

    setting up specs

---
+++ spec/terminal_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe Translatinator do
+  xit "" do
+
+    expect(result).to eq()
+  end
+
+  xit "" do
+
+    expect(result).to eq()
+  end
+
+  xit "" do
+    expect(result).to eq()
+  end
+
+  xit "" do
+    expect(result).to eq()
+  end
+
+  xit "" do
+    expect(result).to eq()
+  end
+
+  xit "" do
+    expect(result).to eq()
+  end
+end
